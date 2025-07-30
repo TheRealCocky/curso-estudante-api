d97e469c985bb1fef689fbaa78554c7d8cb6bc5b
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2025-07-30 01:46:16 +0100

    Corrigir erro de parêntese na string de conexão

--- Program.cs
+++ Program.cs
@@ -4,24 +4,20 @@ using WebApplication1.Routes;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Usa a string de conexão da variável de ambiente
 builder.Services.AddDbContext<Context>(options =>
-    options.UseNpgsql(builder.Configuration["ConnectionStrings__DefaultConnection"]);
-
+    options.UseNpgsql(builder.Configuration["ConnectionStrings__DefaultConnection"]));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Aplica migrations automaticamente
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<Context>();
     db.Database.Migrate();
 }
 
-// Swagger em dev ou produção
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
@@ -34,6 +30,7 @@ app.EstudanteRoutes();
 app.CursoRoutes();
 
 app.Run();
+
 
 
 
